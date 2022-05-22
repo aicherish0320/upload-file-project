7933bbe5b2303275a2af1afde206f31a22cd8590
aicherish <aicherish0320@163.com>
2022-05-22 22:23:54 +0800

    gif 图片类型判断

--- front/pages/UserCenter.vue
+++ front/pages/UserCenter.vue
@@ -38,7 +38,46 @@ export default {
       if (!file) return
       this.file = file
     },
+    async blobToString(blob) {
+      return new Promise((resolve) => {
+        const reader = new FileReader()
+        reader.onload = function () {
+          const ret = reader.result
+            .split('')
+            .map((v) => {
+              const r = v.charCodeAt()
+              return r
+            })
+            .map((v) => {
+              const r = v.toString(16).toUpperCase()
+              return r
+            })
+            // .map(v => v.padStart(2, '0'))
+            .join('')
+
+          resolve(ret)
+        }
+        reader.readAsBinaryString(blob)
+      })
+    },
+    async isGif(file) {
+      // 前面6个16进制, '47 49 46 38 39 61' '47 49 46 38 37 61'
+      // 16 进制的转换
+      const ret = await this.blobToString(file.slice(0, 6))
+      const isGif = ['474946383961', '474946383761'].indexOf(ret) > -1
+      return isGif
+    },
+    async isImage(file) {
+      // 通过文件流来判定
+      // 1. 先判定 gif
+      return await this.isGif(file)
+    },
     async uploadFile() {
+      if (!(await this.isImage(this.file))) {
+        this.$message.warning('文件格式不正确')
+        return
+      }
+
       const form = new FormData()
       form.append('file', this.file)
       const ret = await this.$http.post('/uploadFile', form, {
@@ -48,6 +87,9 @@ export default {
           )
         },
       })
+      if (ret.code === 0) {
+        this.$message.success('图片上传成功')
+      }
     },
     bindEvents() {
       const drag = this.$refs.drag
