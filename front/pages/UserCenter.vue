dce80c288809cf4e424612e88c2d838ba2595542
aicherish <aicherish0320@163.com>
2022-05-22 21:27:32 +0800

    文件上传进度条添加、拖拽上传

--- front/pages/UserCenter.vue
+++ front/pages/UserCenter.vue
@@ -1,7 +1,19 @@
 <template>
   <div>
     <h1>UserCenter</h1>
-    <input type="file" name="file" @change="handleFileChange" />
+
+    <div ref="drag" id="drag">
+      <input type="file" name="file" @change="handleFileChange" />
+    </div>
+
+    <div>
+      <el-progress
+        :stroke-width="20"
+        :text-inside="true"
+        :percentage="uploadProgress"
+      ></el-progress>
+    </div>
+
     <el-button @click="uploadFile">上传</el-button>
   </div>
 </template>
@@ -12,10 +24,13 @@ export default {
   data() {
     return {
       file: null,
+      uploadProgress: 0,
     }
   },
   async mounted() {
     await this.$http.get('/user/info')
+
+    this.bindEvents()
   },
   methods: {
     handleFileChange(e) {
@@ -26,10 +41,42 @@ export default {
     async uploadFile() {
       const form = new FormData()
       form.append('file', this.file)
-      const ret = await this.$http.post('/uploadFile', form)
+      const ret = await this.$http.post('/uploadFile', form, {
+        onUploadProgress: (progress) => {
+          this.uploadProgress = Number(
+            ((progress.loaded / progress.total) * 100).toFixed(2)
+          )
+        },
+      })
+    },
+    bindEvents() {
+      const drag = this.$refs.drag
+      drag.addEventListener('dragover', (e) => {
+        drag.style.borderColor = 'red'
+        e.preventDefault()
+      })
+      drag.addEventListener('drop', (e) => {
+        const fileList = e.dataTransfer.files
+        this.file = fileList[0]
+        e.preventDefault()
+      })
+      drag.addEventListener('dragleave', (e) => {
+        drag.style.borderColor = '#eee'
+        e.preventDefault()
+      })
     },
   },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#drag {
+  height: 100px;
+  line-height: 100px;
+  border: 2px dashed #eee;
+  text-align: center;
+  // &:hover {
+  //   border-color: red;
+  // }
+}
+</style>
