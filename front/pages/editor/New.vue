bb1471f86fec29e360bf2e8409c2d9a8e1f70f46
aicherish <aicherish0320@163.com>
2022-05-28 21:41:41 +0800

    代码高亮

--- front/pages/editor/New.vue
+++ front/pages/editor/New.vue
@@ -5,7 +5,12 @@
     </div>
     <el-row>
       <el-col :span="12">
-        <textarea class="md-editor" :value="content" @input="update"></textarea>
+        <textarea
+          ref="editor"
+          class="md-editor"
+          :value="content"
+          @input="update"
+        ></textarea>
       </el-col>
       <el-col :span="12">
         <div class="markdown-body" v-html="compiledContent"></div>
@@ -16,11 +21,14 @@
 
 <script>
 import { marked } from 'marked'
+import hljs from 'highlight.js'
+import javascript from 'highlight.js/lib/languages/javascript'
+import 'highlight.js/styles/monokai-sublime.css'
 export default {
   name: 'NewEditor',
   data() {
     return {
-      content: '# Marked in Node.js\n\nRendered by **marked**.',
+      content: '# Marked in Node.js',
     }
   },
   computed: {
@@ -28,10 +36,34 @@ export default {
       return marked.parse(this.content)
     },
   },
+  mounted() {
+    this.timer = null
+    this.bindEvents()
+
+    marked.setOptions({
+      renderer: new marked.Renderer(),
+      highlight: function (code, lang) {
+        const language = hljs.getLanguage(lang) ? lang : 'plaintext'
+        return hljs.highlight(code, { language }).value
+      },
+    })
+  },
   methods: {
+    bindEvents() {
+      this.$refs.editor.addEventListener('paste', async (e) => {
+        const files = e.clipboardData.files
+      })
+      this.$refs.editor.addEventListener('drop', (e) => {
+        console.log('e >>> ', e.dataTransfer.files)
+        e.preventDefault()
+      })
+    },
     submit() {},
     update(e) {
-      this.content = e.target.value
+      clearTimeout(this.timer)
+      this.timer = setTimeout(() => {
+        this.content = e.target.value
+      }, 300)
     },
   },
 }
