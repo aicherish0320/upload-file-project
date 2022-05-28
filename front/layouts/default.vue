d012d70ae4d22ab5f83ccb9aca4512f022b0c3d7
aicherish <aicherish0320@163.com>
2022-05-29 07:30:51 +0800

    首页布局

---
+++ front/layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-container>
+    <el-header>
+      <el-menu mode="horizontal">
+        <el-menu-item>登录</el-menu-item>
+        <el-menu-item>退出</el-menu-item>
+        <el-menu-item>写文章</el-menu-item>
+      </el-menu>
+    </el-header>
+    <el-main>
+      <nuxt />
+    </el-main>
+    <el-footer></el-footer>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: 'default',
+}
+</script>
+
+<style lang="scss" scoped></style>
